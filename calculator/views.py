2f27a07ff7eb6d487c69c789e514fe29d7e6d4d3
Ilya Voloshin <ilyavoloshin20@gmail.com>
2023-02-23 20:58:29 +0700

    news

--- calculator/views.py
+++ calculator/views.py
@@ -34,13 +34,3 @@ def calculator(request, menu = DATA):
         'recipe': recipe
     }
     return render(request, 'calculator/home.html', context)
-
-# Напишите ваш обработчик. Используйте DATA как источник данных
-# Результат - render(request, 'calculator/index.html', context)
-# В качестве контекста должен быть передан словарь с рецептом:
-# context = {
-#   'recipe': {
-#     'ингредиент1': количество1,
-#     'ингредиент2': количество2,
-#   }
-# }
